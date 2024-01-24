505273b10e263b44d8b2ec3a34671fbcee89530e
riyavlog <73346395+riyavlog@users.noreply.github.com>
2024-01-24 19:35:08 +0530

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import Navigation from './component/Navigation'
+
+import './App.css'
+import Herosection from './component/Herosection';
+
+function App() {
+  
+
+  return (
+   <div>
+   <Navigation />
+   <Herosection />
+   </div> 
+  )
+}
+
+export default App
---
+++ src/component/Herosection.jsx
@@ -0,0 +1,46 @@
+const Herosection = () => {
+
+return(
+
+
+<main className="hero container">
+<div className="hero-content">
+<h1>Your Feet <br /> <span className="hightlight">Deserve</span> <br /> The Best</h1>
+<p>container becomes a flex container, and its 
+ children with the class.<br />item become flex items. The 
+ justify-content property aligns items
+</p>
+
+<div className="hero-btn">
+
+
+<button id="shop">Shop Now</button>
+
+<button  id = "Cat">Category</button>
+
+</div>
+
+<div className="Shopping">
+<p>Available on</p>
+</div>
+
+</div>
+
+
+<div className = "hero-image">
+<img className="img" src = "./images/redshoes.jpg" alt = "shoes"></img>
+
+
+
+</div>
+
+
+
+</main>
+
+)
+
+
+}
+
+export default Herosection;
